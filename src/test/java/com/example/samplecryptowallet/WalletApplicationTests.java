1069b4c27d573235248c176c6b55ce57c0c03edb
abidin berkay <abidinberkay@gmail.com>
2022-01-26 22:07:49 +0300

    waitforit eklendi

--- src/test/java/com/example/samplecryptowallet/WalletApplicationTests.java
+++ src/test/java/com/example/samplecryptowallet/WalletApplicationTests.java
@@ -1,27 +1,27 @@
-package com.example.samplecryptowallet;
-
-import com.example.samplecryptowallet.Service.UserService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WalletApplicationTests {
-
-    @Autowired
-    UserService userService;
-
+//package com.example.samplecryptowallet;
+//
+//import com.example.samplecryptowallet.Service.UserService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WalletApplicationTests {
+//
+////    @Autowired
+////    UserService userService;
+//
 //	@Test
 //	void contextLoads() {
 //		System.out.println("sd");
 //	}
-
-//    @Test
-//    void userCreate() {
-//        User dto = new User();
-//        dto.setUsername("camomoooo");
-//        userService.create(dto);
-//        assert
-//    }
-
-}
+//
+////    @Test
+////    void userCreate() {
+////        User dto = new User();
+////        dto.setUsername("camomoooo");
+////        userService.create(dto);
+////        assert
+////    }
+//
+//}
