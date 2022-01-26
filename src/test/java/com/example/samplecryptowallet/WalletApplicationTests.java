6642475edd5c0a2980552b30fd51895c6b9f669f
abidin berkay <abidinberkay@gmail.com>
2022-01-26 22:41:48 +0300

    ali hemşoooooooo

--- src/test/java/com/example/samplecryptowallet/WalletApplicationTests.java
+++ src/test/java/com/example/samplecryptowallet/WalletApplicationTests.java
@@ -1,27 +1,13 @@
-//package com.example.samplecryptowallet;
-//
-//import com.example.samplecryptowallet.Service.UserService;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class WalletApplicationTests {
-//
-////    @Autowired
-////    UserService userService;
-//
-//	@Test
-//	void contextLoads() {
-//		System.out.println("sd");
-//	}
-//
-////    @Test
-////    void userCreate() {
-////        User dto = new User();
-////        dto.setUsername("camomoooo");
-////        userService.create(dto);
-////        assert
-////    }
-//
-//}
+package com.example.samplecryptowallet;
+
+import com.example.samplecryptowallet.Service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+	@Test
+	void contextLoads() {}
+}
